23fa03035126dc5ff3e7fc4b3a34c58ffa80094a
malistov.artem <malistov.artem@russianpost.ru>
2022-11-26 23:32:37 +0300

    Add remarks in Task002

--- Seminar001/HomeWork/Task001/src/Main.java
+++ Seminar001/HomeWork/Task001/src/Main.java
@@ -3,18 +3,24 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args)
     {
+        int num = num_input();
+        System.out.printf("Сумма чисел от 1 до %d = %d\n", num, n_sum(num));
+        System.out.printf("Произведение чисел от 1 до %d = %d\n", num, n_factorial(num));
+        n_factorial(num);
+    }
+
+    public static int num_input() {
         Scanner in = new Scanner(System.in);
         boolean check = true;
+        Integer n = null;
         while (check) {
             System.out.print("Введите целое число больше 0: ");
             try {
-                int n = Integer.parseInt(in.nextLine());
+                n = Integer.parseInt(in.nextLine());
                 if (n < 1) {
                     System.out.println("Ошибка! Необходимо вводить целое число больше 0");
                 }
                 else {
-                    System.out.printf("Сумма чисел от 1 до %d = %d\n", n, n_sum(n));
-                    System.out.printf("Произведение чисел от 1 до %d = %d\n", n, n_factorial(n));
                     check = false;
                 }
             }
@@ -22,6 +28,7 @@ public class Main {
                 System.out.println("Ошибка! Необходимо вводить целое число больше 0");
             }
         }
+        return n;
     }
 
     public static int n_sum(Integer args) {
