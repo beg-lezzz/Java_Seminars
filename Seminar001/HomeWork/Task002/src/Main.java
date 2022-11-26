969cdf37ec15ec658b6a41f985e65563bf3d1da7
malistov.artem <malistov.artem@russianpost.ru>
2022-11-26 23:25:16 +0300

    Add remarks in Task001

--- Seminar001/HomeWork/Task002/src/Main.java
+++ Seminar001/HomeWork/Task002/src/Main.java
@@ -5,17 +5,21 @@ public class Main {
 
     public static void main(String[] args)
     {
+        checkPrime(num_input());
+    }
+
+    public static int num_input() {
         Scanner in = new Scanner(System.in);
         boolean check = true;
+        Integer n = null;
         while (check) {
             System.out.print("Введите целое число больше 0: ");
             try {
-                int n = Integer.parseInt(in.nextLine());
+                n = Integer.parseInt(in.nextLine());
                 if (n < 1) {
                     System.out.println("Ошибка! Необходимо вводить целое число больше 0");
                 }
                 else {
-                    checkPrime(n);
                     check = false;
                 }
             }
@@ -23,25 +27,26 @@ public class Main {
                 System.out.println("Ошибка! Необходимо вводить целое число больше 0");
             }
         }
+        return n;
     }
 
-    public static void checkPrime(int args) {
+    public static void checkPrime(int num) {
         ArrayList<Integer> arrayList = new ArrayList<>();
-        for (int i = 2; i <= args; i++) {
-            boolean checkPrime = true;
+        for (int i = 2; i <= num; i++) {
+            boolean check_Prime = true;
             for (int j = 2; j < i; j++) {
                 if(i % j == 0){
-                    checkPrime = false;
+                    check_Prime = false;
                     break;}
             }
-            if(checkPrime)
+            if(check_Prime)
                 arrayList.add(i);
         }
         if (arrayList.size() > 0) {
-            System.out.printf("Список простых чисел от 1 до %d => %s", args, arrayList);
+            System.out.printf("Список простых чисел от 1 до %d => %s", num, arrayList);
         }
         else {
-            System.out.printf("В диапазоне от 1 до %d простые числа отсутствуют", args);
+            System.out.printf("В диапазоне от 1 до %d простые числа отсутствуют", num);
         }
     }
 }
