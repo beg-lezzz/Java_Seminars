3eaae48e7fbb623621fdbb702192c376a9976d00
malistov.artem <malistov.artem@russianpost.ru>
2022-12-02 16:14:50 +0300

    Add remarks in Seminar002 Task002

--- Seminar002/HomeWork/Task002/src/Main.java
+++ Seminar002/HomeWork/Task002/src/Main.java
@@ -4,6 +4,10 @@
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.time.LocalDateTime;
 import java.util.Arrays;
 
@@ -11,38 +15,57 @@ public class Main {
     private static FileWriter writer;
 
     public static void main(String[] args) throws IOException {
-        try {
-            File logger = new File("bubbleSort.log");
-            logger.createNewFile();
-            writer = new FileWriter(logger);
-            Integer[] ARRAY = {5, 29, 45, 0, -1};
+            Integer[] ARRAY = {5, 109, -145, -20, 10};
             bubbleSort(ARRAY);
-        }
-        catch (Exception e) {e.getLocalizedMessage();}
-        finally {writer.close();}
     }
 
     public static void bubbleSort(Integer[] inputArray) throws IOException {
+
         System.out.printf("Первоначальный массив ===> %s\n", Arrays.toString(inputArray));
+        String logMessage = "Первоначальный массив ===> " + Arrays.toString(inputArray) + "\n";
+        logSortOperations(logMessage);
         for (int i = 0; i < inputArray.length - 1; i++){
             for(int j = 0; j < inputArray.length - 1; j++){
                 if (inputArray[j] > inputArray[j + 1]){
                     int tmp = inputArray[j];
                     inputArray[j] = inputArray[j + 1];
                     inputArray[j + 1] = tmp;
-                    logSortOperations(inputArray[j], inputArray[j+1], inputArray);
+                    logMessage = "" + LocalDateTime.now() + " " + inputArray[j + 1] + " <=> " + inputArray[j] + " ===> "
+                                    + Arrays.toString(inputArray) + "\n";
+                    logSortOperations(logMessage);
                 }
             }
         }
         System.out.printf("Отсортированный массив ===> %s\n", Arrays.toString(inputArray));
-        System.out.println("Записи логов успешно произведены");
+        logMessage = "Отсортированный массив ===> " + Arrays.toString(inputArray) + "\n\n";
+        logSortOperations(logMessage);
+        System.out.println("Все ходы записаны и хранятся в файле bubbleSort.log");
     }
 
-    public static void logSortOperations(Integer i, Integer j, Integer[] inputArray) throws IOException {
-        StringBuilder logString = new StringBuilder();
-        logString.append(i).append(" <=> ").append(j).append(" ===> ").append(Arrays.toString(inputArray));
+    public static void logSortOperations(String logMessage) throws IOException {
         try {
-            writer.append(String.valueOf(LocalDateTime.now())).append(" ").append(String.valueOf(logString)).append("\n");
-        } catch (Exception e) {e.getLocalizedMessage();}
+            File log = new File("bubbleSort.log");
+            log.createNewFile();
+        } catch (IOException e) {
+            System.out.println("Упс. Что-то пошло не так. Не удалось обратиться к файлу.");
+            e.printStackTrace();
+        }
+
+        Path path = Paths.get("bubbleSort.log");
+//        String logMessage = "" + i + " <=> " + j + " ===> " + Arrays.toString(inputArray) + "\n";
+
+        try {
+            Files.write(path, logMessage.getBytes(), StandardOpenOption.APPEND);
+//            System.out.println("Это успех. Все ходы записаны.");
+        } catch (IOException e) {
+            System.out.println("Упс. Что-то пошло не так. Лог не записан.");
+            e.printStackTrace();
+        }
+
+//        StringBuilder logString = new StringBuilder();
+//        logString.append(i).append(" <=> ").append(j).append(" ===> ").append(Arrays.toString(inputArray));
+//        try {
+//            writer.append(String.valueOf(LocalDateTime.now())).append(" ").append(String.valueOf(logString)).append("\n");
+//        } catch (Exception e) {e.getLocalizedMessage();}
     }
 }
