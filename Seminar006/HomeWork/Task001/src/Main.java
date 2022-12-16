2134e4ce98616ce07980a17e95e0301fa6ce8f7e
malistov.artem <malistov.artem@russianpost.ru>
2022-12-16 20:21:07 +0300

    Add remarks in Seminar006 Task001

--- Seminar006/HomeWork/Task001/src/Main.java
+++ Seminar006/HomeWork/Task001/src/Main.java
@@ -1,5 +1,8 @@
+import java.util.HashMap;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         NoteBook noteBook1 = new NoteBook(1, 256, 500, "HP", "Pavilion",
                 "Windows 7", "black");
         NoteBook noteBook2 = new NoteBook(1, 512, 1000, "Lenovo", "P3120",
@@ -27,7 +30,89 @@ public class Main {
 
 
 
-        System.out.println(NoteBook.getByRam(noteBooks, 512, 1024));
-        System.out.println(NoteBook.getByOS(noteBooks, "Windows 11"));
+//        System.out.println(NoteBook.getByRam(noteBooks, 512, 1024));
+//        System.out.println(NoteBook.getByOS(noteBooks, "Windows 11"));
+        userMenu(noteBooks);
+
+
+    }
+
+    private static void userMenu(NoteBook[] noteBooks) throws InterruptedException {
+        HashMap<String, String> phoneBook = new HashMap<>();
+        Scanner in = new Scanner(System.in);
+        boolean exit = false;
+        while (!exit){
+            Thread.sleep(1000);
+            System.out.print("\n*** Меню ***\n0. Выйти\n1. Поиск по RAM\n2. Поиск по HDD\n" +
+                    "3. Поиск по цвету\n4. Поиск по операционной системе\n******\n\nВведите номер действия из списка: ");
+            String choice = in.nextLine();
+            switch (choice){
+                case "0":
+                    System.out.println("\nСпасибо, что воспользовались нашим сервисом. Ждем Вас снова.");
+                    exit = true;
+                    break;
+                case "1":
+                    System.out.println("Введите минимальный объем: ");
+                    int min = Integer.parseInt(in.nextLine());
+                    System.out.println("Введите максимальный объем: ");
+                    int max = Integer.parseInt(in.nextLine());
+                    loadImitation();
+                    StringBuilder result = NoteBook.getByRam(noteBooks, min, max);
+                    if (result.length()>0){
+                        System.out.printf("\nНоутбуки с объемом RAM от %d до %d:\n\n%s", min, max, result);
+                    } else {
+                        System.out.printf("Ноутбуки с объемом RAM от %d до %d не найдены.\n", min, max);
+                    }
+                    break;
+                case "2":
+                    System.out.println("Введите минимальный объем: ");
+                    min = Integer.parseInt(in.nextLine());
+                    System.out.println("Введите максимальный объем: ");
+                    max = Integer.parseInt(in.nextLine());
+                    loadImitation();
+                    result = NoteBook.getByHD(noteBooks, min, max);
+                    if (result.length()>0){
+                        System.out.printf("\nНоутбуки с объемом HDD от %d до %d:\n\n%s", min, max, result);
+                    } else {
+                        System.out.printf("Ноутбуки с объемом HDD от %d до %d не найдены.\n", min, max);
+                    }
+                    break;
+                case "3":
+                    System.out.println("Введите цвет: ");
+                    String colour = in.nextLine();
+                    loadImitation();
+                    result = NoteBook.getByColour(noteBooks, colour);
+                    if (result.length()>0){
+                        System.out.printf("\nНоутбуки цвета %s:\n\n%s", colour, result);
+                    } else {
+                        System.out.printf("Ноутбуки цвета %s не найдены.\n", colour);
+                    }
+                    break;
+                case "4":
+                    System.out.println("Введите операцтонную систему: ");
+                    String os = in.nextLine();
+                    loadImitation();
+                    result = NoteBook.getByColour(noteBooks, os);
+                    if (result.length()>0){
+                        System.out.printf("\nНоутбуки с операционной системой %s:\n\n%s", os, result);
+                    } else {
+                        System.out.printf("Ноутбуки с операционной системой %s не найдены.\n", os);
+                    }
+                    break;
+                default:
+                    System.out.println("\nВыбрано некорректное значение. Попробуйте снова.\n");
+            }
+        }
+    }
+
+    private static void loadImitation() throws InterruptedException {
+        System.out.printf("\nПожалуйста, подождите. Мы работаем над этим.\n");
+        Thread.sleep(500);
+        for (int i = 0; i < 60; i++) {
+            System.out.print("\u2591");
+            Thread.sleep(30);
+        }
+        System.out.print(" 100% \n");
+        Thread.sleep(500);
     }
 }
